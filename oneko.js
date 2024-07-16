324d3a665296357c17b1fc11bde3a2de4e369a78
Dan4oVeliki <daani9000@gmail.com>
2024-07-16 16:08:29 +0300

    26

--- oneko.js
+++ oneko.js
@@ -20,7 +20,7 @@
   let idleAnimation = null;
   let idleAnimationFrame = 0;
 
-  const nekoSpeed = 100;
+  const nekoSpeed = 26;
   const spriteSets = {
     idle: [[-3, -3]],
     alert: [[-7, -3]],
